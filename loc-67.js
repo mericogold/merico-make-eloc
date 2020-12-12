381b0866b5a311f6bb9127b869984c7dc6571b9a
mericogold <github4merico+gold@s.gonze.com>
2020-12-12 11:17:35 -0800

    LOC 67. No idea what ELOC this will give me.

--- loc-67.js
+++ loc-67.js
@@ -1,23 +1,5 @@
 /* @flow */
 
-import { install } from './install'
-import { START } from './util/route'
-import { assert } from './util/warn'
-import { inBrowser } from './util/dom'
-import { cleanPath } from './util/path'
-import { createMatcher } from './create-matcher'
-import { normalizeLocation } from './util/location'
-import { supportsPushState } from './util/push-state'
-import { handleScroll } from './util/scroll'
-
-import { HashHistory } from './history/hash'
-import { HTML5History } from './history/html5'
-import { AbstractHistory } from './history/abstract'
-
-import type { Matcher } from './create-matcher'
-
-import { isNavigationFailure, NavigationFailureType } from './util/errors'
-
 export default class VueRouter {
   static install: () => void
   static version: string
@@ -82,33 +64,4 @@ export default class VueRouter {
     return this.history && this.history.current
   }
 
-  init (app: any /* Vue component instance */) {
-    process.env.NODE_ENV !== 'production' &&
-      assert(
-        install.installed,
-        `not installed. Make sure to call \`Vue.use(VueRouter)\` ` +
-          `before creating root instance.`
-      )
-
-    this.apps.push(app)
-
-    // set up app destroyed handler
-    // https://github.com/vuejs/vue-router/issues/2639
-    app.$once('hook:destroyed', () => {
-      // clean out app from this.apps array once destroyed
-      const index = this.apps.indexOf(app)
-      if (index > -1) this.apps.splice(index, 1)
-      // ensure we still have a main app or null if no apps
-      // we do not release the router so it can be reused
-      if (this.app === app) this.app = this.apps[0] || null
-
-      if (!this.app) this.history.teardown()
-    })
-
-    // main app previously initialized
-    // return as we don't need to set up new history listener
-    if (this.app) {
-      return
-    }
-  }
 }
